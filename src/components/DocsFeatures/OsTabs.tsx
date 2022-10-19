d610524f0db9deca8f485a7487ce9c6d3c8f0a37
jamiegoh <jamiegoh16@gmail.com>
2022-10-20 00:31:43 +0800

    Revert "Update OsTabs with conditional props"
    This reverts commit dad03f162bbe6acff876789e652b703c221cdee4.

--- src/components/DocsFeatures/OsTabs.tsx
+++ src/components/DocsFeatures/OsTabs.tsx
@@ -1,24 +1,18 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import Tabs from '@theme/Tabs';
 import TabItem from '@theme/TabItem';
 
-interface OsTabsProps {
-  windows?: ReactNode;
-  macos?: ReactNode;
-  linux?: ReactNode;
-}
-
-const OsTabs = ({windows, macos, linux}: OsTabsProps) => {
+const OsTabs = ({windows, macos, linux}) => {
   return <Tabs groupId="operating-systems">
-    {windows ? <TabItem value="win" label="Windows">
+    <TabItem value="win" label="Windows">
       {windows}
-    </TabItem> : null}
-    {macos ? <TabItem value="mac" label="macOS">
+    </TabItem>
+    <TabItem value="mac" label="macOS">
       {macos}
-    </TabItem> : null}
-    {linux ? <TabItem value="linux" label="Linux">
+    </TabItem>
+    <TabItem value="linux" label="Linux">
       {linux}
-    </TabItem> : null}
+    </TabItem>
   </Tabs>
 }
 
