890539c59323f98d64fff8dc99aba7b5498dd2a1
Jamie Goh <91946833+jamiegoh@users.noreply.github.com>
2022-10-21 00:52:03 +0800

    Add SCSE logos  (#20)
    * Changed icon to scse club icon
    
    * replace favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon-32x32.png',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -60,7 +60,7 @@ const config = {
         title: 'NTU SCSE',
         logo: {
           alt: 'NTU SCSE Logo',
-          src: 'img/logo.svg',
+          src: 'img/scselogo.png',
         },
         items: [
           {
